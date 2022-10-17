35edc1dfc63a7a917b35a82995efd00ec20a5414
Hannah Ton <hannaht2006@gmail.com>
2022-10-17 09:41:19 -0400

    updated Utilities folder

--- src/test/java/com/cydeo/test/TC1.java
+++ src/test/java/com/cydeo/test/TC1.java
@@ -1,5 +1,14 @@
 package com.cydeo.test;
 
-public class TC1 {
+public class TC1 extends TestBase {
+
 
 }
+/*
+US :  Users access to correct modules
+Environment:  https://login2.nextbasecrm.com/
+Steps:: Users are on the homepage
+            Users click a module
+            Verify users see the correct title for the module
+
+ */
